0eaf019fffc334cfd844fb97a356301a2bca5e33
meimei <2931195032@qq.com>
2019-05-20 21:02:50 +0800

    voter

--- app/api/v1/voter.py
+++ app/api/v1/voter.py
@@ -1,13 +1,34 @@
 from flask import jsonify, g
 
-from app.libs.error_code import DeleteSuccess, AuthFailed
+from app.libs.error_code import DeleteSuccess, AuthFailed, Success
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
 from app.models.base import db
+from app.models.votelist import Votelist
 from app.models.voter import Voter
-
+from app.models.voterin import Voterin
 
 api = Redprint('user')
+
+@api.route('/listget', methods=['GET'])
+@auth.login_required
+def get_list():
+    uid = g.voter.uid
+    datas = {}
+    vls = db.session.query(Voterin.vl_id,Votelist.name,Votelist.year,Votelist.votetype,Votelist.votestatus,Votelist.votenum).filter(
+        Voterin.voter_id==uid,Voterin.voterinstatus==0).filter(Voterin.vl_id==Votelist.vl_id).all()
+    lists = []
+    for vl in vls:
+        list ={}
+        list['votelist_name'] = vl[1]
+        list['votelist_year'] = vl[2]
+        list['votelist_votetype'] = vl[3]
+        list['votelist_votestatus'] = vl[4]
+        list['votelist_votenum'] = vl[5]
+        lists.append(list)
+    print(lists)
+
+    return Success(msg='查询列表成功')
 
 
 @api.route('/<int:uid>', methods=['GET'])
