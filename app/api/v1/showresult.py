8c0e31c7615882262ebd8f9d66f2b8ffa498d750
meimei <2931195032@qq.com>
2019-05-21 17:54:47 +0800

    showresult

--- app/api/v1/showresult.py
+++ app/api/v1/showresult.py
@@ -1,3 +1,5 @@
+import fractions
+
 from flask import  request
 from app.libs.error_code import  Success
 from app.libs.redprint import Redprint
@@ -11,28 +13,36 @@ api = Redprint('show_result')
 @api.route('/show',methods=['POST'])
 def add_students():
     jsonData = request.get_json()
+    x = fractions.Fraction(2, 3)
 
     if int(jsonData['type']) ==1:
         results = db.session.query(Excellentresult.s_id,Excellentresult.agreenum,Excellentresult.disagreenum,Excellentresult.abstained,
                          Masterstudents.name,Masterstudents.account,Masterstudents.major,Masterstudents.title,Masterstudents.tutor,
                          Masterstudents.college,Masterstudents.thesisurl
-                         ).filter(Excellentresult.s_id==Masterstudents.s_id,Excellentresult.vl_id==jsonData['vl_id']
-                         ).filter(Excellentresult.agreenum>(Excellentresult.agreenum+Excellentresult.disagreenum+Excellentresult.abstained)*2/3).all()
+                         ).filter(Excellentresult.s_id==Masterstudents.s_id,Excellentresult.vl_id==jsonData['vl_id'],).all()
+
         list = []
         for re in results:
             result={}
+
             result['name'] = jsonData['name']
-            result['Excellentresult.s_id'] = re[0]
-            result['Excellentresult.agreenum'] = re[1]
-            result['Excellentresult.disagreenum'] = re[2]
-            result['Excellentresult.abstained'] = re[3]
-            result['Masterstudents.name'] = re[4]
-            result['Masterstudents.account'] = re[5]
-            result['Masterstudents.major'] = re[6]
-            result['Masterstudents.title'] = re[7]
-            result['Masterstudents.tutor'] = re[8]
-            result['Masterstudents.college'] = re[9]
-            result['Masterstudents.thesisurl'] = re[10]
+            result['s_id'] = re[0]
+            result['agreenum'] = re[1]
+            result['disagreenum'] = re[2]
+            result['abstained'] = re[3]
+            result['name'] = re[4]
+            result['account'] = re[5]
+            result['major'] = re[6]
+            result['title'] = re[7]
+            result['tutor'] = re[8]
+            result['college'] = re[9]
+            result['thesisurl'] = re[10]
+            result['pass'] = 0
+
+            if re[1]>(re[1]+re[2]+re[3])*x:
+                result['pass'] = 1
+            else:
+                result['pass'] = 0
             list.append(result)
 
 
